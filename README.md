2264afde1f8b8652b970f93276f5a98e84102b8a
iuill <jhdkoiiondt@yahoo.co.jp>
2021-08-30 19:10:12 +0900

    first commit.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # DC_PerformanceVisualizer
-企業型DCのパフォーマンス可視化用ツール
+
+企業型確定拠出年金(DC)の銘柄ごとのパフォーマンスを可視化するツール
+
+## 作った経緯
+
+公式サイトの機能では銘柄ごとのパフォーマンスが見れなかったため
+
+## 構成
+
+当方の環境では以下構成で構築
+
+- PowerAutomateDesktopでサイトをスクレイピングし、結果をjson形式で保存
+- 本ツールで当jsonデータを読み込み、グラフ化
+
+## 依存関係
+
+- Grid.js
+- Chart.js
+
+## LICENSE
+
+[License](/LICENSE "License")
---
+++ fundResult.js
@@ -0,0 +1,8 @@
+
+dat = [
+{name:"ファンドA", hyoukagaku:"140,000", syutokugaku:"130,000", son_eki:"10,000"},
+{name:"ファンドB", hyoukagaku:"330,000", syutokugaku:"430,000", son_eki:"-100,000"},
+{name:"ファンドC", hyoukagaku:"500,000", syutokugaku:"300,000", son_eki:"200,000"},
+{name:"ファンドD", hyoukagaku:"400,000", syutokugaku:"350,000", son_eki:"50,000"},
+{name:"ファンドE", hyoukagaku:"300,000", syutokugaku:"300,000", son_eki:"0"},
+]
